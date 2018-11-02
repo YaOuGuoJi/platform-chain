bcb228ef95004ca10fac55a9ec43b68cc297cdd6
zh <277708272@qq.com>
2018-11-02 16:58:50 +0800

    建立常量类

---
+++ src/main/java/com/bester/platform/platformchain/constant/OreRecordSource.java
@@ -0,0 +1,10 @@
+package com.bester.platform.platformchain.constant;
+
+public class OreRecordSource {
+
+    /**
+     * 日常领取
+     */
+    public static final int DAILY_RECEIVE = 1;
+
+}
---
+++ src/main/java/com/bester/platform/platformchain/constant/PowerSource.java
@@ -0,0 +1,41 @@
+package com.bester.platform.platformchain.constant;
+
+public class PowerSource {
+
+    /**
+     * 注册
+     */
+    public static final int REGISTER = 1;
+
+    /**
+     * 购物
+     */
+    public static final int SHOPPING = 2;
+
+    /**
+     * 游戏
+     */
+    public static final int GAMING = 3;
+
+    /**
+     * 小说
+     */
+    public static final int FICTION = 4;
+
+    /**
+     * 新闻
+     */
+    public static final int NEWS = 5;
+
+    /**
+     * 邀请好友
+     */
+    public static final int INVITE = 6;
+
+    /**
+     * 关注公众号
+     */
+    public static final int PUBLIC = 7;
+
+
+}

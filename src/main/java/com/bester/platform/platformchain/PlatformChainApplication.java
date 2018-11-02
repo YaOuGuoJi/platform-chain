9e2d99db78f27e0992fe86189832dd7902ff469c
YanRuis <1131411057@qq.com>
2018-11-02 11:23:30 +0800

    init

---
+++ src/main/java/com/bester/platform/platformchain/PlatformChainApplication.java
@@ -0,0 +1,12 @@
+package com.bester.platform.platformchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformChainApplication.class, args);
+	}
+}

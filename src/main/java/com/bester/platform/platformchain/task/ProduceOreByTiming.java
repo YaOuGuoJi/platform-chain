8d18c1955ad23d2d0acb077cb013bacb2f420c2a
zh <277708272@qq.com>
2018-11-03 09:21:46 +0800

    save

--- src/main/java/com/bester/platform/platformchain/task/ProduceOreByTiming.java
+++ src/main/java/com/bester/platform/platformchain/task/ProduceOreByTiming.java
@@ -1,4 +1,4 @@
-package com.bester.platform.platformchain.timedtask;
+package com.bester.platform.platformchain.task;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;

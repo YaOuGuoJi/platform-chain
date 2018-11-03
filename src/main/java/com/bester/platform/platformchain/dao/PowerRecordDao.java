367426822e13937a176a13b64a990201f05a6851
Morse <1185585454@qq.com>
2018-11-03 15:43:08 +0800

    save

--- src/main/java/com/bester/platform/platformchain/dao/PowerRecordDao.java
+++ src/main/java/com/bester/platform/platformchain/dao/PowerRecordDao.java
@@ -3,6 +3,7 @@ package com.bester.platform.platformchain.dao;
 import com.bester.platform.platformchain.entity.PowerEntity;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -10,13 +11,22 @@ import java.util.List;
  * @date 2018/11/2
  */
 public interface PowerRecordDao {
-    // 在这里新增接口
 
     /**
-     * 查询用户有效算力记录
+     * 查询用户有效的算力记录
      *
-     * @param userId
+     * @param userId 用户id
+     * @param time 有效临时算力的添加时间起始值
      * @return
      */
-    List<PowerEntity> selectPowerRecordByUserId(@Param("userId") int userId);
+    List<PowerEntity> selectUserValidPower(@Param("userId") int userId, @Param("time") Date time);
+
+    /**
+     * 查询用户已失效算力记录
+     *
+     * @param userId 用户id
+     * @param time 失效临时算力的添加时间截止值
+     * @return
+     */
+    List<PowerEntity> selectUserExpiredPower(@Param("userId") int userId, @Param("time") Date time);
 }

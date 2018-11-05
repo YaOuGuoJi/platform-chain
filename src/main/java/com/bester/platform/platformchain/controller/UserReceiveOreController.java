6c9d2840a16266419a88ff85bcd4832e8020bb93
Liang <1144351205@qq.com>
2018-11-05 17:34:08 +0800

    修改格式

--- src/main/java/com/bester/platform/platformchain/controller/UserReceiveOreController.java
+++ src/main/java/com/bester/platform/platformchain/controller/UserReceiveOreController.java
@@ -18,32 +18,34 @@ import java.util.List;
 public class UserReceiveOreController {
     @Resource
     private OreRecordService oreRecordService;
+
     @GetMapping("user/unreceived/ore")
     public CommonResult showOre(Integer userId) {
         List<OreRecordDTO> oreRecordDTOs = oreRecordService.showOreByUserId(userId);
-        if(CollectionUtils.isEmpty(oreRecordDTOs)){
-            return CommonResult.fail(404,"参数错误或者没有可以收的矿");
+        if (CollectionUtils.isEmpty(oreRecordDTOs)) {
+            return CommonResult.fail(404, "参数错误或者没有可以收的矿");
         }
         return CommonResult.success(oreRecordDTOs);
     }
+
     @GetMapping("user/received/ore")
-    public CommonResult receiveOre(Integer userId,Integer oreId){
+    public CommonResult receiveOre(Integer userId, Integer oreId) {
         OreRecordDTO oreRecordDTO = oreRecordService.showOreById(oreId);
-        if(oreRecordDTO==null){
-            return CommonResult.fail(404,"没有这条记录");
+        if (oreRecordDTO == null) {
+            return CommonResult.fail(404, "没有这条记录");
         }
-        if(!oreRecordDTO.getUserId().equals(userId)){
-            return CommonResult.fail(403,"此矿不是你的矿");
+        if (!oreRecordDTO.getUserId().equals(userId)) {
+            return CommonResult.fail(403, "此矿不是你的矿");
         }
-        if(oreRecordDTO.getStatus().equals(1)){
-            return CommonResult.fail(403,"此矿已收");
+        if (oreRecordDTO.getStatus().equals(1)) {
+            return CommonResult.fail(403, "此矿已收");
         }
-        if(oreRecordDTO.getStatus().equals(0)){
-            return CommonResult.fail(403,"此矿已过期");
+        if (oreRecordDTO.getStatus().equals(0)) {
+            return CommonResult.fail(403, "此矿已过期");
         }
-        Integer sta = oreRecordService.receiveOre(oreId,userId);
-        if(sta.equals(0)){
-            return CommonResult.fail(403,"收矿失败");
+        Integer sta = oreRecordService.receiveOre(oreId, userId);
+        if (sta.equals(0)) {
+            return CommonResult.fail(403, "收矿失败");
         }
         return CommonResult.success();
     }

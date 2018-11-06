0d0bcd800d5f743f5c146d3e3c8f987be0e9d759
Morse <1185585454@qq.com>
2018-11-06 16:27:47 +0800

    fix

---
+++ src/main/java/com/bester/platform/platformchain/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.bester.platform.platformchain.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @author liuwen
+ * @date 2018/11/6
+ */
+@RestController
+public class LoginController {
+
+
+}

4c28c0e926cb2bb5a4621b735e224b93954a2ba4
zh <277708272@qq.com>
2018-12-03 16:15:44 +0800

    fix

--- src/main/java/com/bester/platform/platformchain/controller/FindPageController.java
+++ src/main/java/com/bester/platform/platformchain/controller/FindPageController.java
@@ -4,14 +4,12 @@ import com.bester.platform.platformchain.common.CommonResult;
 import com.bester.platform.platformchain.constant.RedisKeys;
 import com.bester.platform.platformchain.enums.HttpStatus;
 import com.bester.platform.platformchain.service.RedisClientService;
-import com.google.common.collect.Maps;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Map;
 
 /**
  * @author zhangqiang
@@ -33,9 +31,7 @@ public class FindPageController {
         if (CollectionUtils.isEmpty(shoppingList)) {
             return CommonResult.fail(HttpStatus.NOT_FOUND);
         }
-        Map<String, List<Object>> result = Maps.newHashMap();
-        result.put(RedisKeys.FIND_PAGE_SHOPPING_KEY, shoppingList);
-        return CommonResult.success(result);
+        return CommonResult.success(shoppingList);
     }
 
 }

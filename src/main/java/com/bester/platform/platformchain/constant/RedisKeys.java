6c08fa98c5f9ae3b7e4dfdd05d913d44b1e5df33
Morse <1185585454@qq.com>
2018-12-03 10:52:14 +0800

    fix

--- src/main/java/com/bester/platform/platformchain/constant/RedisKeys.java
+++ src/main/java/com/bester/platform/platformchain/constant/RedisKeys.java
@@ -6,6 +6,12 @@ package com.bester.platform.platformchain.constant;
  */
 public class RedisKeys {
 
+    /**
+     * 矿石总产量
+     */
     public static final String TOTAL_ORE_NUMBER_KEY = "totalOreNumber";
+    /**
+     * 每日矿石产量
+     */
     public static final String DAY_ORE_NUMBER_KEY = "dayOreNumber_";
 }

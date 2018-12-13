6bfe4b7fb30f12c7318cdf79f0fc4a18f9729970
Morse <1185585454@qq.com>
2018-12-13 18:48:49 +0800

    fix

--- src/test/java/com/bester/platform/platformchain/dao/VoucherCardDaoTest.java
+++ src/test/java/com/bester/platform/platformchain/dao/VoucherCardDaoTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 
 /**
@@ -28,8 +29,10 @@ public class VoucherCardDaoTest {
         List<VoucherCardEntity> entityList = Lists.newArrayList();
         for (int i = 0; i < 1000; i++) {
             String cardId = "VO01" + RandomUtil.getStringRandom(12);
+            cardId = cardId.replaceAll("O", "0");
             VoucherCardEntity voucherCardEntity = new VoucherCardEntity();
             voucherCardEntity.setCardId(cardId);
+            voucherCardEntity.setAmount(new BigDecimal("1000.00"));
             entityList.add(voucherCardEntity);
         }
         int result = voucherCardDao.addCard(entityList);

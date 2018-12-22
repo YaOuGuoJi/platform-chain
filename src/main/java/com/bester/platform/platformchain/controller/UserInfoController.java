a26102c472fb090c783e0b459f5deb41a69eb7da
zh <277708272@qq.com>
2018-12-22 14:54:01 +0800

    修改编辑个人信息接口

--- src/main/java/com/bester/platform/platformchain/controller/UserInfoController.java
+++ src/main/java/com/bester/platform/platformchain/controller/UserInfoController.java
@@ -1,7 +1,6 @@
 package com.bester.platform.platformchain.controller;
 
 import com.bester.platform.platformchain.common.CommonResult;
-import com.bester.platform.platformchain.dto.UserAccountDTO;
 import com.bester.platform.platformchain.dto.UserInfoDTO;
 import com.bester.platform.platformchain.dto.VoucherCardDTO;
 import com.bester.platform.platformchain.enums.HttpStatus;
@@ -10,19 +9,16 @@ import com.bester.platform.platformchain.service.UserInfoService;
 import com.bester.platform.platformchain.service.VoucherCardService;
 import com.bester.platform.platformchain.util.UserInfoUtil;
 import lombok.Data;
-import org.springframework.beans.BeanUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.util.CollectionUtils;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * @author liuwen
@@ -33,16 +29,15 @@ public class UserInfoController {
 
     @Resource
     private UserInfoService userInfoService;
-    @Resource
-    private UserAccountService userAccountService;
+
     @Resource
     private VoucherCardService voucherCardService;
 
     @PostMapping("/user/updateInfo")
-    public CommonResult updateUserInfo(UserInfoVO userInfoVO) {
-        CommonResult validResult = validParams(userInfoVO);
-        if (!validResult.isSuccess()) {
-            return validResult;
+    public CommonResult updateUserInfo(String email, String job) {
+        String emailRegex = "^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.[a-zA-Z0-9]{2,6}$";
+        if (!Pattern.matches(emailRegex, email) || StringUtils.isEmpty(job)) {
+            return CommonResult.fail(HttpStatus.PARAMETER_ERROR);
         }
         int userId = UserInfoUtil.getUserId();
         if (userId < 0) {
@@ -50,14 +45,8 @@ public class UserInfoController {
         }
         UserInfoDTO userInfoDTO = new UserInfoDTO();
         userInfoDTO.setUserId(userId);
-        BeanUtils.copyProperties(userInfoVO, userInfoDTO);
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        try {
-            Date birth = sdf.parse(userInfoVO.getBirthday());
-            userInfoDTO.setBirthday(birth);
-        } catch (ParseException e) {
-            return CommonResult.fail(403, "生日输入不合法！");
-        }
+        userInfoDTO.setEmail(email);
+        userInfoDTO.setJob(job);
         int result = userInfoService.updateUserInfo(userInfoDTO);
         return result > 0 ? CommonResult.success() : CommonResult.fail(HttpStatus.ERROR);
     }
@@ -80,64 +69,11 @@ public class UserInfoController {
         return CommonResult.success(membership);
     }
 
-    private CommonResult validParams(UserInfoVO userInfoVO) {
-        if (userInfoVO == null || StringUtils.isEmpty(userInfoVO.getUserName())) {
-            return CommonResult.fail(403, "用户名不得为空！");
-        }
-        int userId = UserInfoUtil.getUserId();
-        UserAccountDTO userAccountDTO = userAccountService.findUserAccountInfoByPhoneNum(userInfoVO.phone);
-        if (userAccountDTO != null && userId != userAccountDTO.getUserId()) {
-            return CommonResult.fail(403, "该手机号已经注册！");
-        }
-        if (userInfoVO.sex == null || userInfoVO.sex <= 0) {
-            return CommonResult.fail(403, "性别不合法");
-        }
-        return CommonResult.success();
-    }
-
     private BigDecimal plusAmount(List<VoucherCardDTO> voucherCardDTOList) {
         if (CollectionUtils.isEmpty(voucherCardDTOList)) {
             return BigDecimal.ZERO;
         }
         return voucherCardDTOList.stream().map(VoucherCardDTO::getAmount).reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    @Data
-    public class UserInfoVO {
-        /**
-         * 用户姓名
-         */
-        private String userName;
-
-        /**
-         * 性别 1-男 2-女 其他-未知
-         */
-        private Integer sex;
-
-        /**
-         * 生日 yyyy-MM-dd
-         */
-        private String birthday;
-
-        /**
-         * 手机号
-         */
-        private String phone;
-
-        /**
-         * 邮箱
-         */
-        private String email;
-
-        /**
-         * 地址
-         */
-        private String address;
-
-        /**
-         * 职业/工作
-         */
-        private String job;
     }
 
     @Data

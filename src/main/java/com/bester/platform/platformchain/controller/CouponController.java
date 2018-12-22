61bd9f83b257c4cf814e940dcc63f12e1e70a4ad
zh <277708272@qq.com>
2018-12-22 12:15:18 +0800

    修改请求方式

--- src/main/java/com/bester/platform/platformchain/controller/CouponController.java
+++ src/main/java/com/bester/platform/platformchain/controller/CouponController.java
@@ -16,6 +16,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -41,7 +42,7 @@ public class CouponController {
      * @param couponId
      * @return
      */
-    @GetMapping("/user/receive/coupon")
+    @PostMapping("/user/receive/coupon")
     public CommonResult receiveCoupon(Integer couponId) {
         if (couponId == null) {
             return CommonResult.fail(403, "参数错误");

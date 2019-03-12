f9fff3a3c484dbd193a76787ccfb2760fcfe9326
Morse <1185585454@qq.com>
2019-03-12 11:29:01 +0800

    fix

--- src/main/java/com/bester/platform/platformchain/controller/lastrip/CharactersController.java
+++ src/main/java/com/bester/platform/platformchain/controller/lastrip/CharactersController.java
@@ -12,24 +12,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CharactersController {
 
-    private static final String TEST_PRIVATE_KEY = "5K9veg5tG7fhsPepfvg8y8eRwMTLPwMTsJURDMUa52ZpZPBCCKc";
-    private static final String TEST_ACCOUNT_NAME = "morse1113";
-    private static final String TEST_POINT = "https://testnet.gxchain.org";
-    private static final String TEST_CONTRACT = "genesis-lw";
+    private static final String PRIVATE_KEY = "5KTE1uL3fdtkuzsHye3y8ej3gxF1aGQY2Los6W8veNrfvgCCnUe";
+    private static final String ACCOUNT_NAME = "matrixdapp1";
+    private static final String NET_POINT = "https://node1.gxb.io";
+    private static final String CONTRACT = "genesis";
 
-    private static GXChainClient gxChainClient = new GXChainClient(TEST_PRIVATE_KEY, TEST_ACCOUNT_NAME, TEST_POINT);
+    private static GXChainClient gxChainClient = new GXChainClient(PRIVATE_KEY, ACCOUNT_NAME, NET_POINT);
 
     @GetMapping("/genesis/characterCount")
     public String characterCount() {
         final String table = "characount";
-        JsonElement characterCount = gxChainClient.getTableRows(TEST_CONTRACT, table, 0, 1);
+        JsonElement characterCount = gxChainClient.getTableRows(CONTRACT, table, 0, 1);
         return characterCount.toString();
     }
 
     @GetMapping("/genesis/character")
     public String characters(int id) {
         final String table = "character";
-        JsonElement characterCount = gxChainClient.getTableRows(TEST_CONTRACT, table, id, 1);
+        JsonElement characterCount = gxChainClient.getTableRows(CONTRACT, table, id, 1);
         return characterCount.toString();
     }
 }
